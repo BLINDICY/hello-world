a788a85418f360b5b632d74b3c5025be764d299e
BLINDICY <34733573+BLINDICY@users.noreply.github.com>
2018-01-10 23:55:27 -0500

    Add files via upload

---
+++ Sample.py 4.py
@@ -0,0 +1,19 @@
+spam = 0				
+if spam == 10:
+    print('eggs')
+    if spam > 5:
+        print('bacon')
+    else:
+        print('ham')
+    print('spam')
+print('spam')
+
+spam = 1
+if spam == 1:
+    print('Hello')
+    if spam == 2:
+        print('Howdy')
+    else:
+        print('Greetings!')
+    print('spam')
+
