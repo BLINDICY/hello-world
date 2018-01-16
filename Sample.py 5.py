d0881e7c7470cf18601a6bb271b30f41d94fceef
BLINDICY <34733573+BLINDICY@users.noreply.github.com>
2018-01-16 13:44:25 -0500

    Add files via upload

---
+++ Sample.py 5.py
@@ -0,0 +1,9 @@
+# print even or odd in a range of numbers
+
+
+for number in range(50):
+    if number%2==0:
+        print(number, " is an even number")
+    elif number %2==1:
+        print(number, " is an odd number")
+
