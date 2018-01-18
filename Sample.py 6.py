8b15a33c9f3f17b3c88c0c9382d55702f5f07921
BLINDICY <34733573+BLINDICY@users.noreply.github.com>
2018-01-18 13:42:08 -0500

    Add files via upload

---
+++ Sample.py 6.py
@@ -0,0 +1,9 @@
+'''
+for number in range(30):
+    if number%2 == 0:
+        print(number, "is an even number")
+'''
+
+for number in range(15,30):
+    if number%2 == 0:
+        print(number, "is an even number")
